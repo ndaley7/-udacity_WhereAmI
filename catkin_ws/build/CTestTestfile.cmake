6fae21ce1a39c79a281f17a69329fcb438f9dd52
Nathan <nathan.in.daley@gmail.com>
2019-06-19 11:16:09 -0700

    AMCL Workgin
    odom is now showing in rvis and I am able to adjust robot parameters.

--- catkin_ws/build/CTestTestfile.cmake
+++ catkin_ws/build/CTestTestfile.cmake
@@ -5,8 +5,6 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 subdirs(gtest)
-subdirs(my_robot)
 subdirs(teleop_twist_keyboard)
-subdirs(ball_chaser)
-subdirs(pgm_map_creator)
 subdirs(udacity_bot)
+subdirs(pgm_map_creator)
